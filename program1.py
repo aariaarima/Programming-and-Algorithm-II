8be3a50a45c135c37410a41841a703deb8c23785
aarima <108648547+aariaarima@users.noreply.github.com>
2023-05-12 10:07:37 +0545

    Programming

---
+++ program1.py
@@ -0,0 +1,35 @@
+# #WAP to print the fibonacci series
+# n=int(input("Enter a number: "))
+# a=0
+# b=1
+# for i in range(1, n-1):
+#     sum = a+b
+#     a=b
+#     b=sum
+
+# print (sum)
+    
+
+
+#WAP to check wheather the entered number is armstong or not
+# a=int(input("Enter a number: "))
+# b=len(str(a))
+# num=0
+# for i in str(a):
+#     num+=int(i) ** 3
+
+# if num==int(a):
+#     print ("The number is armstrong.")
+# else:
+#     print ("The number is not armstrong.")
+
+
+
+
+#WAP to find the factorial of any number input though the keyboard
+n=int(input("Enter a number: "))
+f=1
+for i in range(1, n+1):
+    f=f*i
+print (f)
+
